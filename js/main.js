2f5ff0338d77cdc7ef758156c3e7bf33925001d7
alexBortsov <bortsov78saltek@gmail.com>
2022-11-14 00:11:55 +0200

    14.11.22 0:11

--- js/main.js
+++ js/main.js
@@ -4,7 +4,7 @@ $(document).ready(function(){
         slidesToScroll: 3,
         autoplay: true,
         autoplaySpeed: 2000,
-        prevArrow: '<img class="arrow-left" src="images/back.png" alt="prevArrow">',
-        nextArrow: '<img class="arrow-right" src="images/next.png" alt="nextArrow">',
+        prevArrow: 'none',
+        nextArrow: 'none',
     });
   });
